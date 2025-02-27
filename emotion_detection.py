007a605b4b2ff0c48acd732818739f2442dabb69
“get-bit” <maya.chump_0n@icloud.com>
2025-02-27 11:36:45 -0500

    response formatting added

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,6 +3,14 @@ import requests
 
 def emotion_detector(text_to_analyze):
     '''This function uses the Watson NLP libraries to return emotion reading'''
+    # Initiate emotion scores
+    anger_score = None
+    disgust_score = None
+    fear_score = None
+    joy_score = None
+    sadness_score = None
+    dominant_emotion = None
+
     # Define the URL for the emotion analysis API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     # Set the headers with the required model ID for the API
@@ -13,5 +21,24 @@ def emotion_detector(text_to_analyze):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=input_json, headers=headers)
 
-    # Return the text
-    return {response.text}
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+    emotions_response = formatted_response['emotionPredictions'][0]
+    emotion_dict = emotions_response['emotion']
+    anger_score = emotion_dict['anger']
+    disgust_score = emotion_dict['disgust']
+    fear_score = emotion_dict['fear']
+    joy_score = emotion_dict['joy']
+    sadness_score = emotion_dict['sadness']
+    dominant_emotion = max(emotion_dict)
+    new_dict = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
+
+    # Return the parsed response
+    return new_dict
